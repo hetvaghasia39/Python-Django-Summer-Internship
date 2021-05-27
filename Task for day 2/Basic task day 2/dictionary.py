5bd5fa9baad949e6cbae88ca61a0d09d085bf446
Het Vaghasia <64800864+hetvaghasia39@users.noreply.github.com>
2021-05-27 09:32:35 +0530

    Basic python task
    task for day 2

---
+++ Task for day 2/Basic task day 2/dictionary.py
@@ -0,0 +1,12 @@
+# dictionies examples
+
+d = {1 : 'Tony', 2 : 'Het', 3 : 'Wanda', 'Bruce' : 4, 'Natasha' : 5}
+
+# type() function
+print(type(d))
+
+print(d)
+
+print(d[3])
+
+print(d["Natasha"])
---
+++ Task for day 2/Basic task day 2/variable.py
@@ -0,0 +1,29 @@
+#variable example
+a = 5
+b = 12.5
+name = 'Het Vaghasia'
+
+print(a)
+print("b = ",b)
+print("name: ",name)
+
+#changing value example
+heroName = "Thor Odinson"
+print("hero name: ",heroName)
+
+heroName = "Captain America"
+print("hero name after changing: ",heroName)
+
+#assigning multiple value example
+c, d, city = 12, 4.5, "new york"
+print("c: ",c)
+print("d: ",d)
+print("city: ",city)
+
+# assigning same value to multiple variable
+e = f = g = 20
+
+print("e: ",e)
+print("f: ",f)
+print("g: ",g)
+
