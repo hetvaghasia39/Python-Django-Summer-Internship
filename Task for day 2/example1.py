71599391fd0a12151cc048b002404fa9a4962307
Het Vaghasia <64800864+hetvaghasia39@users.noreply.github.com>
2021-05-27 10:14:17 +0530

    day 2
    task 2

---
+++ Task for day 2/example1.py
@@ -0,0 +1,7 @@
+print("Hi this is Het Vaghasia")
+
+#this is single line comment
+
+''' this is 
+    multi line comment
+    Het Vaghasia'''
---
+++ Task for day 2/tuple.py
@@ -0,0 +1,14 @@
+# tuple examples
+
+tuple1 = (10, 20, 30, "Tony", "Het", "Sacrlet", "Loki")
+
+print(tuple1)
+
+print("7th element: ",tuple1[6])
+
+print("starting element to 3rd: ",tuple1[:3])
+
+print("4th to last element: ",tuple1[3:])
+
+# tuples are immutable
+tuple1[4] = "Hulk"
